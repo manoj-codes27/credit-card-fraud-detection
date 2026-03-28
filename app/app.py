992a0120eb8181bdc0012ff3b9b98a89fa00a8c8
AKULA MANOJ <manojakula27@gmail.com>
2026-03-28 14:02:57 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,17 +3,18 @@ import numpy as np
 import pickle
 
 # Load trained model
+scaler = pickle.load(open("app/scaler.pkl", "rb"))
 model = pickle.load(open("app/fraud_model.pkl", "rb"))
 
 st.title("💳 Credit Card Fraud Detection")
 
 st.write("Enter transaction details to predict whether it's fraud or not")
 
-# Example inputs (simplified)
+# Inputs
 amount = st.number_input("Transaction Amount", min_value=0.0)
 time = st.number_input("Transaction Time", min_value=0.0)
 
-# Dummy inputs for V1–V28 (since dataset is anonymized)
+# V1–V28 inputs
 v_features = []
 for i in range(1, 29):
     val = st.number_input(f"V{i}", value=0.0)
@@ -22,7 +23,9 @@ for i in range(1, 29):
 # Prediction
 if st.button("Predict"):
     input_data = np.array([time, amount] + v_features).reshape(1, -1)
-    
+
+    input_data = scaler.transform(input_data)
+
     prediction = model.predict(input_data)
 
     if prediction[0] == 1:
